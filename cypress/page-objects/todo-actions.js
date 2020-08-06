2458027f6e90037bec51818a93ab8b6a5400d163
8abc <anadelaine.catalla@gmail.com>
2020-08-05 18:49:49 -0700

    page opjects in cypress

--- cypress/page-objects/todo-actions.js
+++ cypress/page-objects/todo-actions.js
@@ -1,12 +1,37 @@
 /// <reference types="cypress" />
 
-import { TodoPage } from "../page-objects/todo-page";
+import {
+  navigate,
+  addTodo,
+  validateTodoText,
+  toggleTodo,
+  clearCompleted,
+  validateTodoCompletedState,
+  validateToggleState,
+  validateNumberOfTodosShown,
+} from "../page-objects/todo-page";
 
 describe("todo actions", () => {
-  const TodoPage = new TodoPage();
-
   beforeEach(() => {
-    todoPage.navigate();
-    todoPage.addTodo("Wash dishes");
+    navigate();
+    addTodo("Clean room");
+  });
+
+  it("should add a new todo to the list", () => {
+    validateTodoText(0, "Clean room");
+    validateToggleState(0, false);
+  });
+
+  describe("toggling todos", () => {
+    it("should toggle test correctly", () => {
+      toggleTodo(0);
+      validateTodoCompletedState(0, true);
+    });
+
+    it("should clear completed", () => {
+      toggleTodo(0);
+      clearCompleted();
+      validateNumberOfTodosShown(0);
+    });
   });
 });
